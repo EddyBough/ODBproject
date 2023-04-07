e95bbbba9553b7b5a2ce2d2b1016afc832a9ce57
EddyBough <boughanmieddy@outlook.fr>
2023-04-07 12:14:40 +0200

    versionapi

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -1,6 +1,7 @@
 const { customerModel } = require("../models/customerModel");
 const customerRouter = require("express").Router();// Constante pour créer un routeur qui a pour nom customerRouter
 const crypto = require('../service/crypto')
+const axios = require('axios')
 
 //-----------------------------------Page Home------------------------------------------------------------------
 
@@ -14,50 +15,82 @@ customerRouter.get('/home', async (req, res) => {// le get permet d'afficher la 
     }
 })
 
+customerRouter.get('/test', async (req, res) => {// le get permet d'afficher la page d'inscription (register)
+    try {
+        const headers = {
+            'Authorization': `Bearer ${process.env.API_KEY}`,
+          };
+        let res = await axios.get(`https://calendly.com/api/v1/users/me`, {
+            headers: headers
+          })
+
+         
+          console.log(res);
+        res.render("home.twig",{
+
+        })
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+})
+
+
 //-----------------------------------Page Connexion--------------------------------------------------------------
 
 customerRouter.get('/login', async (req, res) => {// le get permet d'afficher la page de connexion (register)
     try {
-        res.render("login.twig")
-    } catch (error) {
-        console.log(error);
-        res.send(error)
-    }
-})
-
-
-customerRouter.post('/login', async (req,res)=>{// le post va envoyer les donnée qu'on a entré dans le formulaire de connexion
-    try {
-        let customer = await customerModel.findOne({mail: req.body.mail}) // le findOne va rechercher un email sur mongoDB qui correspond ou pas à l'email entré par l'utilisateur
-        if (customer && await crypto.comparePassword(req.body.password, customer.password)){ //Si l'email correspond le comparePassword va comparer le mot de passe dans la BDD 
-        req.session.customer = customer //L'utilisateur sera récupéré
-    res.redirect('/dashboard')// Il sera redirigé vers le dashboard
+        let errorLogin = ""
+        if (req.session.errorLogin) {
+            errorLogin = req.session.errorLogin
+            delete req.session.errorLogin
         }
+        res.render("login.twig", {
+            errorLogin: errorLogin
+        })
     } catch (error) {
         console.log(error);
         res.send(error)
     }
-    
 })
+
+customerRouter.post('/login', async (req, res) => {// le post va envoyer les donnée qu'on a entré dans le formulaire de connexion
+
+    try {
+        let customer = await customerModel.findOne({ mail: req.body.mail }) // le findOne va rechercher un email sur mongoDB qui correspond ou pas à l'email entré par l'utilisateur
+        if (customer && await crypto.comparePassword(req.body.password, customer.password)) { //Si l'email correspond le comparePassword va comparer le mot de passe dans la BDD 
+            req.session.customer = customer //L'utilisateur sera récupéré
+            res.redirect('/dashboard')// Il sera redirigé vers le dashboard
+        } else {
+            throw 'non connecté'
+        }
+
+    } catch (error) {
+        console.log(error);
+        req.session.errorLogin = error
+        res.redirect('/login')
+    }
+})
+
+
 
 //-----------------------------------Page déconnexion du Dashboard--------------------------------------------------------------
 
 customerRouter.get("/logout", async (req, res) => {//
     try {
         req.session.destroy() //Le destroy va récupérer l'id en cours et va le detruire afin de se deconnecter
-    res.redirect("/login")// Il redige par la suite à la page login
+        res.redirect("/login")// Il redige par la suite à la page login
     } catch (error) {
         console.log(error);
         res.send(error)
     }
-    
 });
 
 //-----------------------------------Fonction supprimer compte--------------------------------------------------------------
 
 customerRouter.get("/customerDelete/:id", async (req, res) => {
     try {
-        await customerModel.deleteOne({_id: req.params.id }) //Il va recuperer l'id de la BDD afin de le supprimer
+        await customerModel.deleteOne({ _id: req.params.id }) //Il va recuperer l'id de la BDD afin de le supprimer
         res.redirect("/login"); //Ensuite il le redirige vers la page connexion
     } catch (error) {
         console.log(error);
@@ -93,18 +126,18 @@ customerRouter.get('/register', async (req, res) => {// le get permet d'afficher
     }
 })
 
-customerRouter.post('/register', async (req,res)=>{// Le post lui permet d'ajouter un client dans la base de donnée
+customerRouter.post('/register', async (req, res) => {// Le post lui permet d'ajouter un client dans la base de donnée
     try {
-    req.body.password = await crypto.cryptPassword(req.body.password)//Await est utilisé pour attendre la fin de l'éxécution du crypto.
-    //le crypto permet lui de crypter le password afin qu'il ne soit pas visible dans la BDD
-    let customer = new customerModel(req.body);
-    customer.save();// Le client est enregistré dans la BDD
-    res.redirect('/dashboard')//Il sera ensuite redirigé vers son dashboard
+        req.body.password = await crypto.cryptPassword(req.body.password)//Await est utilisé pour attendre la fin de l'éxécution du crypto.
+        //le crypto permet lui de crypter le password afin qu'il ne soit pas visible dans la BDD
+        let customer = new customerModel(req.body);
+        customer.save();// Le client est enregistré dans la BDD
+        res.redirect('/dashboard')//Il sera ensuite redirigé vers son dashboard
     } catch (error) {
         console.log(error);
         res.send(error)
     }
-    
+
 })
 
 
@@ -113,20 +146,20 @@ customerRouter.post('/register', async (req,res)=>{// Le post lui permet d'ajout
 
 
 
-customerRouter.get('/dashboard', async (req, res)=>{
+customerRouter.get('/dashboard', async (req, res) => {
     try {
         console.log(req.session);
-        let customers =  await customerModel.find({customerId: req.session.customerId})//le find va recuperer une requete mongoose
-   res.render('dashboard.twig',{ // je suis bien authentifié mon tableau de bord client apparait
-    customers: customers,//la ligne de code customers: customers est un tableau d'objet et permet de donner une étiquette aux données que nous souhaitons afficher dans la vue, 
-    //afin que nous puissions les utiliser facilement dans le code Twig pour afficher les informations du client.
-    connectedCustomer: req.session.customer//connectedCustomer est égal à la session sur laquelle l'utilisateur est connecté
-})
+        let customers = await customerModel.find({ customerId: req.session.customerId })//le find va recuperer une requete mongoose
+        res.render('dashboard.twig', { // je suis bien authentifié mon tableau de bord client apparait
+            customers: customers,//la ligne de code customers: customers est un tableau d'objet et permet de donner une étiquette aux données que nous souhaitons afficher dans la vue, 
+            //afin que nous puissions les utiliser facilement dans le code Twig pour afficher les informations du client.
+            connectedCustomer: req.session.customer//connectedCustomer est égal à la session sur laquelle l'utilisateur est connecté
+        })
     } catch (error) {
         console.log(error);
         res.json(error)
     }
-    
+
 });
 
 //-----------------------------------Page Modification des clients-------------------------------------------------
@@ -135,8 +168,8 @@ customerRouter.get('/dashboard', async (req, res)=>{
 
 customerRouter.get('/modificationProfil/:id', async (req, res) => {
     try {
-        let customer = await customerModel.findOne({_id: req.params.id})
-        res.render("modificationProfil.twig",{
+        let customer = await customerModel.findOne({ _id: req.params.id })
+        res.render("modificationProfil.twig", {
             customer: customer
         })
     } catch (error) {
@@ -145,14 +178,14 @@ customerRouter.get('/modificationProfil/:id', async (req, res) => {
     }
 })
 
-customerRouter.post("/modificationProfil/:id", async (req, res)=>{
+customerRouter.post("/modificationProfil/:id", async (req, res) => {
     try {
-        await customerModel.updateOne(({_id: req.params.id}), req.body)
+        await customerModel.updateOne(({ _id: req.params.id }), req.body)
         res.redirect("/dashboard");
     } catch (error) {
         console.log(error);
         res.send(error);
-        
+
     }
 })
 
